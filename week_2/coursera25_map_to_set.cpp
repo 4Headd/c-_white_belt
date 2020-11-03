0fbef834ad31a1110c2a98eba970eab83fe47eed
4Headd <43352077+4Headd@users.noreply.github.com>
2020-11-03 16:36:34 +0300

    map to set

---
+++ week_2/coursera25_map_to_set.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+#include <string>
+#include <map>
+
+using namespace std;
+
+set<string> BuildMapValuesSet(const map<int,string>& m){
+	set<string> s;
+	for(const auto& item : m){
+		s.insert(item.second);
+	}
+	return s;
+}
+
+int main(){
+
+}
