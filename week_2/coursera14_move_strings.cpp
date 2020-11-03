81bdf2884b266cfca3c081c8d16d9b08b6942557
4Headd <43352077+4Headd@users.noreply.github.com>
2020-11-03 16:20:51 +0300

    move strings

---
+++ week_2/coursera14_move_strings.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void MoveStrings(vector<string>& source, vector<string>& destination){
+	for(auto i:source){
+		destination.push_back(i);
+	}
+	source.clear();
+}
+
+int main() {
+	vector<string> source = {"a", "b", "c"};
+	vector<string> destination = {"z"};
+	MoveStrings(source, destination);
+	for(auto i:destination){
+		cout << i << " ";
+	}
+	cout << endl << source.size();
+	return 0;
+}
